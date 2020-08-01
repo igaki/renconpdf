96cc66e540b0316400fb1e6dd2cdbd51e90915d1
Hiroshi Igaki <github@musictale.net>
2020-08-01 22:36:30 +0900

    init files

---
+++ consid2pdf.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+import os
+import img2pdf
+from traceback import print_exc
+from PIL import Image
+import imdirect
+import re
+import struct
+
+# 参考
+# https://kapibara-sos.net/archives/866
+
+
+# PDFをpdfフォルダに作成して，変換元のイメージをcompleteに移動する
+
+
+def save_pdf(path_, item, ext_, dir_path_complete):
+    rep = re.compile(ext_, re.IGNORECASE)  # キャピタライズを無視して
+    img_pdf_bytes = img2pdf.convert(path_)
+    save_pdf_file = re.sub(rep, '.pdf', item)
+    # print("save_pdf_file:"+item+":ext_:"+ext_+":"+save_pdf_file)
+    file = open(dir_path + "/pdf/" +
+                save_pdf_file, "wb")
+    file.write(img_pdf_bytes)
+    file.close()
+    path_complete = os.path.join(dir_path_complete, item)
+    # print("move(save_pdf):"+path_+":"+"path_complete")
+    move_to_complete(path_, path_complete)
+
+
+def convert_png_jpg(path_, item, ext_, dir_path_complete):
+  # pngをjpgに変換する
+    rep = re.compile(ext_, re.IGNORECASE)  # キャピタライズを無視して
+    input_image = Image.open(path_)
+    rgb_image = input_image.convert('RGB')
+    rgb_image_path = re.sub(rep, '.jpg', path_)
+    # print("convert:"+rgb_image_path)
+    rgb_image.save(rgb_image_path, quality=80)
+    input_image.close()
+    path_complete = os.path.join(dir_path_complete, item)
+    # print("move(convert):"+path_+":"+"path_complete"+path_complete)
+    move_to_complete(path_, path_complete)
+    # item名の拡張子をjpgに変換（元のままだとpngのため，save_pdfでバグる）
+    rgb_image_item = re.sub(rep, '.jpg', item)
+    rgb_image_ext = re.sub(rep, '.jpg', ext_)
+
+    save_pdf(rgb_image_path, rgb_image_item, rgb_image_ext, dir_path_complete)
+
+
+def move_to_complete(orig, dest):
+  # ファイルを指定した場所に移動する
+    try:
+        os.rename(orig, dest)
+    except FileExistsError as fee:
+        print(fee)
+        print("orig:"+orig)
+        print("dest:"+dest)
+
+
+def convert_dir(dir_path, dir_path_complete):
+  # 指定したディレクトリ内の画像ファイルをPDFに変換してpdfフォルダに移動する
+    # check input argument
+    if os.path.isdir(dir_path):
+        pdf_file_path = "{}.pdf".format(os.path.basename(dir_path))
+        pdf_file_path = os.path.join(os.path.dirname(dir_path), pdf_file_path)
+    else:
+        raise Exception("invalid path")
+
+    # convertできなかったイメージファイルのpathを入れるリスト
+    images_ = []
+    print("scanning directory: {}".format(dir_path))
+    for item in os.listdir(dir_path):
+        path_ = os.path.join(dir_path, item)
+        if os.path.isfile(path_):
+            ext_ = os.path.splitext(path_)[-1].lower()
+            # print(ext_)
+            try:
+                if ext_ in [".jpg", ".jpeg"]:  # pngを取り除く（アルファチャンネルがどうとかでだめらしい
+                    print("img_pdf:" + path_)
+                    save_pdf(path_, item, ext_, dir_path_complete)
+                elif ext_ in [".png"]:  # png to jpg
+                    convert_png_jpg(path_, item, ext_, dir_path_complete)
+                elif ext_ in [".pdf"]:  # pdf into complete dir
+                    path_pdf = os.path.join(dir_path+'/pdf/', item)
+                    move_to_complete(path_, path_pdf)
+                else:
+                    # skip non-image file
+                    continue
+            except img2pdf.ExifOrientationError as eoe:
+                print(eoe)
+                img_eoe = Image.open(path_)
+                print("{0}, Orientation: {1}".format(
+                    img_eoe, img_eoe._getexif().get(274)))
+                imdirect.monkey_patch()
+                img_eoe_rotated = Image.open(path_)
+                print("{0}, Orientation: {1}".format(
+                    img_eoe_rotated, img_eoe_rotated._getexif().get(274)))
+                img_eoe.close()
+                img_eoe_rotated.close()
+                print("eoe:img_pdf:" + path_)
+
+                save_pdf(path_, item, ext_, dir_path_complete)
+            except TypeError as te:
+                print("TypeError:" + str(te))
+                images_.append(path_)
+            except ValueError as ve:
+                print("ValueError:" + str(ve))
+                images_.append(path_)
+            except struct.error as se:
+                print("struct.error"+str(se))
+                images_.append(path_)
+
+        else:
+            # skip sub directory
+            continue
+
+    return images_
+    # if len(images_) == 0:
+    #     raise Exception("no image files")
+    # else:
+    #     images_.sort()
+
+    # layout_ = img2pdf.get_layout_fun(params_.pagesize_)
+    #print("copying skipped files: ...")
+
+
+if __name__ == "__main__":
+    try:
+        dir_path = "./exama/output"
+        dir_path_pdf = "./exama/output/pdf"
+        dir_path_complete = "./exama/output/complete"
+        try:
+            os.mkdir(dir_path_pdf)
+        except FileExistsError:
+            print(dir_path_pdf+" already exists")
+        try:
+            os.mkdir(dir_path_complete)
+        except FileExistsError:
+            print(dir_path_complete+" already exists")
+
+        images_ = convert_dir(dir_path, dir_path_complete)
+        if len(images_) == 0:
+            print("All files are converted to pdf")
+        else:
+            print(str(len(images_))+" files are not converted")
+            print("Please re-convert again")
+            # プログラム中でコマンド再実行しても駄目だが，プログラム自体を再実行するとだいたいいける
+
+    except BaseException:
+        print_exc()
---
+++ renfile2sid.py
@@ -0,0 +1,125 @@
+import os
+import pandas as pd
+import numpy as np
+import re
+
+
+def name_rdex(df):
+    # 参考
+    # https://qiita.com/tomotaka_ito/items/594ee1396cf982ba9887
+
+    # csvから学生番号と氏名（空白削除して）読み込み，姓名，名姓の順に保存したデータセットを作成する
+
+    name_rd = []  # 名前からスペースを削除したものを格納するリスト
+    name_rdex = []  # 名前からスペースを削除して姓名を反転したものを格納するリスト
+
+    for row in df.itertuples():
+        # print(row)
+        # print(row[2].split())
+        name_space_reduced = ''.join(row[2].split())
+        name_space_reduced_exchanged = row[2].split()[1]+row[2].split()[0]
+        # print(name_space_reduced)
+        name_rd.append(name_space_reduced)
+        name_rdex.append(name_space_reduced_exchanged)
+
+    df['name_rd'] = name_rd  # spaceを削除した名前の列を追加
+    df['name_rdex'] = name_rdex  # spaceを削除して姓名を入れ替えた列を追加
+
+    # print(df)
+    return df
+
+
+def getImages(dir_path):
+
+    # イメージを順に読み込み，画像ファイルのファイル名だけをimages_に格納して返す関数
+
+    images_ = []
+    for item in os.listdir(dir_path):
+        path_ = os.path.join(dir_path, item)
+        if os.path.isfile(path_):
+            ext_ = os.path.splitext(path_)[-1].lower()
+            if ext_ in [".jpg", ".jpeg", ".png", ".pdf"]:
+                # pathだと./exama\\hoge_藤原 健人.jpegのようになる
+                images_.append(item)
+    return images_
+
+
+def getname_fromfile(img_):
+    # イメージファイル名に含まれているユーザ名と拡張子を返す関数
+
+    img_rpd = re.sub('[0-9]+', '', img_.replace(' ', ''))  # 半角スペースと数字を削除
+    under = img_rpd.rfind('_') + 1  # 末尾からみた_の次の文字のindex
+    period = img_rpd.rfind('.')  # 末尾から見た拡張子まで
+    ext = img_rpd[period:len(img_rpd)]
+
+    print("name_from_file:" + img_rpd[under:period])
+    return img_rpd[under:period], ext
+
+
+def rename_file_sid(images_, dir_path, output_path):
+  # ファイル名を学生番号_通し番号.jpgのような形式に変更する
+    for img in images_:
+        img_info = getname_fromfile(img)
+        # print(img_info)
+        ext = img_info[1]
+        mask1 = df['name_rd'] == img_info[0]  # ファイルに含まれるユーザ名と姓名が一致するデータ
+        mask2 = df['name_rdex'] == img_info[0]  # ファイルに含まれるユーザ名と名姓が一致するデータ
+
+        num = 1
+        if (len(df[mask1].index) != 0):
+            name_mask = mask1
+        elif (len(df[mask2].index) != 0):
+            name_mask = mask2
+        else:
+            print(img_info[0] + " has skipped!!!!!!")
+            continue
+        while True:
+            try:
+                # print(type(df.loc[name_mask, 'file' + str(num)]))
+                # print(df.loc[name_mask, 'file' + str(num)])
+                if (df.loc[name_mask, 'file' + str(num)].isnull().any()):
+                    pass
+                else:
+                    num = num + 1
+                    continue
+            except KeyError as ke:
+                print(ke)
+            except TypeError as te:
+                print(te)
+        # ファイル名を学生番号_通し番号のファイル名に変更
+            df.loc[name_mask, 'file' + str(num)] = img
+            ren_img = df.loc[name_mask, 'sid'] + '_' + f'{num:02}' + ext
+            df.loc[name_mask, 'ren_file' + str(num)] = ren_img
+            ren_img_str = df.loc[name_mask, 'ren_file' + str(num)].iat[0]
+
+            img_path = os.path.join(dir_path, img)
+            ren_img_path = os.path.join(output_path, ren_img_str)
+            print("img_path:" + img_path)
+            print("ren_img_path:" + ren_img_path)
+
+            try:
+                os.rename(img_path, ren_img_path)
+            except FileExistsError as fee:
+                print(fee)
+
+            break
+    return df
+
+
+if __name__ == "__main__":
+    # sidと学生名が入力された名簿
+    df = pd.read_csv('./cpu2020.csv', encoding='cp932')
+    df = name_rdex(df)
+    print(df)
+
+    dir_path = "./exama"
+    output_path = "./exama/output"
+
+    try:
+        os.mkdir(output_path)
+    except FileExistsError:
+        print("output dir already exists")
+
+    images = getImages(dir_path)
+    df = rename_file_sid(images, dir_path, output_path)
+    df.to_csv("cpu2020_file.csv", encoding='cp932')
